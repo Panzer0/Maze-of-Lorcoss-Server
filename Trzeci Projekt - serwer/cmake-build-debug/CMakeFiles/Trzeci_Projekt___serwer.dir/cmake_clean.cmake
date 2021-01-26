5a858cf9a1caa40201d1ca5ea6db41508d348e0b
Daniel Wieczorek <57329020+Panzer0@users.noreply.github.com>
2021-01-26 23:28:46 +0100

    Add files via upload

---
+++ Trzeci Projekt - serwer/cmake-build-debug/CMakeFiles/Trzeci_Projekt___serwer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trzeci_Projekt___serwer.dir/main.c.o"
+  "Trzeci_Projekt___serwer"
+  "Trzeci_Projekt___serwer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Trzeci_Projekt___serwer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
